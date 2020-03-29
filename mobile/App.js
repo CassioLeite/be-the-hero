089b200ea2462baabd8e2d763aad14a539824fc4
CassioLeite <cassiodegoes@live.com>
2020-03-29 01:41:39 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import 'intl';3
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
